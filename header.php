f4df8408c3b2d052292355b04273cfed63e23e79
topniz <nizar.ayed@upgrade-code.org>
2018-11-02 10:05:44 +0100

    modif 1

--- header.php
+++ header.php
@@ -1,3 +1,11 @@
+    <style>
+        header {
+            text-align: center,
+        }
+        nav {
+            text-align: center,
+        }
+    </style>
     <header>
         <h1>Catalogue des Films Marvel</h1>
     </header>
