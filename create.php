76033090a2570acdeeda20b5e3eb2e778bb7bf9f
Yann MICHAUX <yann.michaux@centraldev.fr>
2018-11-02 09:30:49 +0100

    Fixing url and date

--- create.php
+++ create.php
@@ -2,7 +2,7 @@
 
 if($_POST) {
     $titre = $_POST['titre'];
-    $date = $_POST['date'];
+    $sortie = $_POST['sortie'];
     $url = $_POST['url'];
     $separator = '<#-#>';
 
@@ -12,14 +12,16 @@ if($_POST) {
 
     $standard_titre = strtolower($titre);
     $standard_titre = str_replace(' ', '-', $standard_titre);
+    $standard_titre = str_replace(':', '', $standard_titre);
     
     $file_id = 'id'.$separator.str_pad(($file_count_read + 1), 5, '0', STR_PAD_LEFT);
+    $file_date = 'date'.$separator.date('d/m/Y - H:i:s');
     $file_title = 'titre'.$separator.$titre;
-    $file_date = 'date'.$separator.$date;
+    $file_sortie = 'sortie'.$separator. $sortie;
     $file_url = 'url'.$separator.$url;
 
     $write_film = fopen('./films/' . $standard_titre . '.txt', 'w+');
-    $write_film_do = fwrite($write_film, $file_id."\r".$file_title."\r".$file_date."\r".$file_url);
+    $write_film_do = fwrite($write_film, $file_id."\r". $file_date . "\r" .$file_title."\r". $file_sortie ."\r".$file_url);
     fclose($write_film);
 
     $update_films_number = fopen('./filecount.txt', 'w+');
@@ -32,13 +34,13 @@ if($_POST) {
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <title>Document</title>
+    <title>Films Marvels</title>
 </head>
 <body>
     <div class="container">
@@ -47,8 +49,8 @@ if($_POST) {
             <label>Titre</label>
             <input type="text" name="titre" class="form-control">
             <hr/>
-            <label>Date</label>
-            <input type="text" name="date" class="form-control">
+            <label>Année de sortie</label>
+            <input type="text" name="sortie" class="form-control">
             <label>Image</label>
             <input type="url" name="url" class="form-control">
             <br/>
--- listing.php
+++ listing.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <title>Document</title>
+    <title>Films Marvel</title>
     <style>
         td {
             vertical-align: middle !important;
@@ -28,8 +28,9 @@
             <thead>
                 <tr>
                     <th>ID</th>
+                    <th>Date d'ajout</th>
                     <th>Titre</th>
-                    <th>Date</th>
+                    <th>Année de sortie</th>
                     <th>Image</th>
                 </tr>
             </thead>
