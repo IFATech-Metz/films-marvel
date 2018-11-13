acea80cbe8b0f10f48e17eb68da7e15efc00d46c
Yann MICHAUX <yann.michaux@centraldev.fr>
2018-11-13 11:36:41 +0100

    Rework project structure

--- index.php
+++ index.php
@@ -1,13 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Films Marvel</title>
-</head>
-<body>
-    <a href="create.php">Création</a><br/>
-    <a href="listing.php">Affichage</a><br/>
-</body>
-</html>
+<?php
+
+require_once('includes/header.php');
+
+switch ($_SERVER['REQUEST_URI']) {
+
+    case '/creation':
+        require_once('pages/create.php');
+        break;
+
+    case '/description':
+        require_once('pages/description.php');
+        break;
+        
+    default:
+        require_once('pages/index.php');
+        break;
+}
+
+?>
