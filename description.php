bc0d789ec75729a4cfbbcc4f6c11dc7cea1169f6
Mathieu Flick <mathieuflick@gmail.com>
2018-12-11 16:42:28 +0100

    modif css description.php

--- description.php
+++ description.php
@@ -2,6 +2,16 @@
 
 require_once('includes/header.php');
 
+// $file = file_get_contents('./resources/movies/'.$_GET['film']);
+// $separator = '<#-#>';
+
+// $movies = [];
+
+// foreach($file_line as $line) {
+//     $file_line_content = explode($separator, $line);
+//     $movies[$file_line_content[0]] = $file_line_content[1];
+// }
+
 $separator = '<#-#>';
 $file = fopen('./resources/movies/'.$_GET['film'], "r");
 $movies = [];
@@ -18,7 +28,7 @@ fclose($file)
         <div>
             <img src="<?php echo $movies['url']; ?>"/>
         </div>
-        <div class= description-text>
+        <div class= description-text> 
             <h3 class="title"><?php echo $movies['titre']; ?></h3>
             <h4>Date de sortie : <?php echo $movies['sortie']; ?></h4>
             <p><?php echo $movies['summary']; ?></p>
