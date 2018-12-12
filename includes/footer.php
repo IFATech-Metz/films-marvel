8a057017fcb571e8924ab9385b4ed6e98ec8b2d3
Yann MICHAUX <yann.michaux@centraldev.fr>
2018-12-12 11:49:44 +0100

    Added copyright

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,7 @@
+    <div class="m-t-100"><strong>&copy; Développé avec  <i class="fas fa-heart" style="color:red"></i>  par <a href="https://github.com/YannMCX" target="_blank" rel="noopener noreferrer">Yann</a>, <a href="https://github.com/ThibaultJamin" target="_blank" rel="noopener noreferrer">Thibault</a> et <a href="https://github.com/MathieuFlick" target="_blank" rel="noopener noreferrer">Mathieu</a></strong></div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <script defer src="https://use.fontawesome.com/releases/v5.5.0/js/all.js" integrity="sha384-GqVMZRt5Gn7tB9D9q7ONtcp4gtHIUEW/yG7h98J7IpE3kpi+srfFyyB/04OV6pG0" crossorigin="anonymous"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.18/datatables.min.js"></script>
